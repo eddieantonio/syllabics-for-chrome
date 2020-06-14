47891ff50cd7753bbfc9b7361f54a14ac861aa46
Eddie Antonio Santos <easantos@ualberta.ca>
2020-06-14 13:15:20 -0600

    copy-paste Google's ime example

--- background.js
+++ background.js
@@ -1,15 +1,3 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({color: '#3aa757'}, () => {})
-
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [
-        new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: { hostEquals: 'syllabics.app' },
-        })
-      ],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }])
-  })
-
+  console.log('Hello, I am installed')
 })
