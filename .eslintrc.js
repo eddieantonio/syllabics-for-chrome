fe598340e1a8be79ffb72264227deefc29312863
Eddie Antonio Santos <easantos@ualberta.ca>
2020-06-14 14:45:48 -0600

    use composition

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,6 @@ module.exports = {
     ecmaVersion: 11,
   },
   rules: {
+    'no-use-before-define': ['error', { functions: false }],
   },
 };
--- main.js
+++ main.js
@@ -5,6 +5,7 @@
 
 const { ime } = chrome.input;
 const INVALID_INPUT_CONTEXT = -1;
+let input;
 
 /**
  * ID of an input target. It becomes invalid on a blur event.
@@ -29,17 +30,32 @@ ime.onBlur.addListener(() => { contextID = INVALID_INPUT_CONTEXT; });
 ime.onKeyEvent.addListener((engineID, keyData) => {
   if (keyData.type === 'keydown' && keyData.key.match(/^[eioa]$/)) {
     // TODO: use chrome.input.setComposition?
-    chrome.input.ime.commitText({
+    chrome.input.ime.setComposition({
       contextID,
-      text: {
-        e: '\u1401',
-        i: '\u1403',
-        o: '\u1405',
-        a: '\u140a',
-      }[keyData.key],
+      text: sro2syllabics(input = keyData.key),
+      cursor: input.length,
     });
     return true;
   }
 
+  if (keyData.type === 'keydown' && keyData.key === ' ') {
+    ime.commitText({ contextID, text: sro2syllabics(input) }, (success) => {
+      if (success) ime.clearComposition({ contextID });
+    });
+
+    /* Let another layer handle the spacebar proper.
+     * this usually means inserting a space! */
+    return false;
+  }
+
   return false;
 });
+
+function sro2syllabics(text) {
+  return {
+    e: '\u1401',
+    i: '\u1403',
+    o: '\u1405',
+    a: '\u140a',
+  }[text.toLowerCase()];
+}
